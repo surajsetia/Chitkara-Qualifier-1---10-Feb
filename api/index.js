391b35650f69818d06ba2b9dad4d71d920255cd9
Suraj <surajsetia.pf@gmail.com>
2026-02-10 12:20:53 +0530

    Prepare project for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -162,7 +162,9 @@ app.post("/bfhl", async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+/*const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+});*/
+
+module.exports = app;
