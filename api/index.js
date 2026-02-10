94c1f22fdc992ed787f4469a30bc7b3b465c3b2a
Suraj <surajsetia.pf@gmail.com>
2026-02-10 12:44:08 +0530

    Updated

--- api/index.js
+++ api/index.js
@@ -167,8 +167,8 @@ const PORT = process.env.PORT || 3000;
   console.log(`Server running on port ${PORT}`);
 });*/
 if (process.env.NODE_ENV !== "production") {
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
